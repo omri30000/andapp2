d54dd0f26501d1eaf58c88212660508f02238c3a
Omri Zaiman <omri162004@gmail.com>
2020-10-19 09:00:39 +0300

    added onClick method to the button

--- app/src/main/java/com/example/andapp2/MainActivity.java
+++ app/src/main/java/com/example/andapp2/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.andapp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int count = 0;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.resultTV);
+    }
+
+    public void methodToDo(View view) {
+        this.count++;
+        String st = getString(R.string.textOnTextBox) + count;
+        tv.setText(st);
     }
 }
